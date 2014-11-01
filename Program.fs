b96cbb4dd57b287cd15ae8819532e99417d6b46d
Jb Evain <jb@evain.net>
2014-11-01 18:37:33 +0100

    Import flispy

---
+++ Program.fs
@@ -0,0 +1,114 @@
+﻿// Scheme interpreter in F#
+// ref: http://norvig.com/lispy2.html
+
+module FLispy.Program
+
+open System
+open System.Collections.Generic
+open System.Globalization
+open System.Reflection
+
+open FLispy.Runtime
+open FLispy.Lexer
+
+let program_env =
+    let bindings =
+        Map.ofList [
+            lookup_symbol("foo"), ref (List([Number(1); Number(2); Number(3);]))
+        ]
+
+    appendFrame bindings global_env
+
+let assert' exp str =
+    let input = parse_string str
+    let res = to_str (eval input program_env)
+    if exp <> res then
+        sprintf "Expected %s got %s" exp res |> Console.WriteLine
+
+assert' "3"              "(length foo)"
+assert' "1"              "(car foo)"
+assert' "(2 3)"          "(cdr foo)"
+assert' "#f"             "(null? foo)"
+assert' "(1 2 3)"        "'(1 2 3)"
+assert' "4"              "(length '(1 2 3 4))"
+assert' "1"              "(if #t 1 0)"
+assert' "0"              "(if #f 1 0)"
+assert' "1"              "(if #t 1)"
+assert' "#t"             "(list? '(1 2))"
+assert' "#f"             "(list? 1)"
+assert' "42"             "(+ 10 20 12)"
+assert' "-4"             "(- 1 2 3)"
+assert' "(1 2 3)"        "(list 1 2 3)"
+assert' "(list 1 2 3)"   "'(list 1 2 3)"
+assert' "12"             "(begin (define x 12) x)"
+assert' "42"             "(begin (define answer 30) (set! answer (+ answer 12))  answer)"
+assert' "9"              "(begin (define square (lambda (x) (* x x))) (square 3))"
+assert' "233"            "
+(begin
+  (define fib
+    (lambda (n)
+      (if (< n 2)
+        n
+        (+ (fib (- n 1)) (fib (- n 2))))))
+  (fib 13))"
+
+assert' "64"             "(square 8)" 
+
+assert' "80"             "
+(begin
+  (define make-account
+    (lambda (balance)
+      (lambda (amt) 
+        (begin
+          (set! balance (+ balance amt))
+          balance))))
+
+  (define account (make-account 100)) 
+  (account -20))"
+
+assert' "()"             "(define twice (lambda (x) (* 2 x)))"
+assert' "()"             "(define compose (lambda (f g) (lambda (x) (f (g x)))))"
+assert' "(10)"           "((compose list twice) 5)"
+assert' "()"             "(define repeat (lambda (f) (compose f f)))"
+assert' "80"             "((repeat (repeat twice)) 5)"
+assert' "()"             "(define abs (lambda (n) ((if (> n 0) + -) 0 n)))"
+assert' "(3 0 3)"        "(list (abs -3) (abs 0) (abs 3))"
+
+assert' "()"             "
+(define combine (lambda (f)
+    (lambda (x y)
+      (if (null? x) (quote ())
+          (f (list (car x) (car y))
+             ((combine f) (cdr x) (cdr y)))))))"
+
+assert' "()"             "(define zip (combine cons))"
+
+assert' "((1 5) (2 6) (3 7) (4 8))"  "(zip (list 1 2 3 4) (list 5 6 7 8))"
+
+assert' "()"             "
+(define riff-shuffle
+  (lambda (deck)
+    (begin
+      (define take
+        (lambda (n seq)
+          (if (<= n 0)
+            (quote ())
+            (cons (car seq) (take (- n 1) (cdr seq))))))
+      (define drop
+        (lambda (n seq)
+          (if (<= n 0)
+            seq
+            (drop (- n 1) (cdr seq)))))
+      (define mid
+        (lambda (seq)
+          (/ (length seq) 2)))
+      ((combine append) (take (mid deck) deck) (drop (mid deck) deck)))))"
+
+assert' "(1 5 2 6 3 7 4 8)"          "(riff-shuffle (list 1 2 3 4 5 6 7 8))"
+assert' "(1 3 5 7 2 4 6 8)"          "((repeat riff-shuffle) (list 1 2 3 4 5 6 7 8))"
+assert' "(1 2 3 4 5 6 7 8)"          "(riff-shuffle (riff-shuffle (riff-shuffle (list 1 2 3 4 5 6 7 8))))"
+
+assert' "()"                         "(define (add x y) (+ x y))"
+assert' "42"                         "(add 40 2)"
+
+assert' "42"                         "(eval '(* 21 2))"
---
+++ Runtime.fs
@@ -0,0 +1,222 @@
+﻿module FLispy.Runtime
+
+open System
+
+type Expression =
+    | Number of int
+    | String of string
+    | Symbol of Symbol
+    | Boolean of bool
+    | Function of (Expression list -> Environment -> Expression)
+    | List of Expression list
+and Symbol = { Name: string }
+and Frame = Map<Symbol, Expression ref> ref
+and Environment = Frame list
+
+let nil = List ([])
+
+let mutable private symbols = Map.empty
+
+let lookup_symbol str =
+    match symbols |> Map.tryFind str with
+    | Some (sym) -> sym
+    | None ->
+        let sym = { Name = str }
+        symbols <- Map.add str sym symbols
+        sym
+
+let lookupRef sym env =
+    match List.tryPick (fun (f: Frame) -> Map.tryFind sym f.Value) env with
+    | Some (ref) -> ref
+    | None -> sprintf "Symbol %s lookup failure" sym.Name |> failwith 
+
+let lookup sym env =
+    let ref = lookupRef sym env
+    ref.Value
+
+let appendFrame frame env =
+    List.append [ref (frame)] env
+
+let rec to_str result =
+    match result with
+    | Boolean (b) -> if b then "#t" else "#f"
+    | Number (n) -> n.ToString ()
+    | String (s) -> "\"" + s + "\""
+    | Symbol (s) -> s.Name
+    | List (l) ->  List.map to_str l |> String.concat " " |> sprintf "(%s)"
+    | Function (_) -> "#<function>"
+
+let rec eval (exp: Expression) (env:Environment) =
+    match exp with
+    | Number (_) | String (_) | Boolean (_) as literal -> literal
+    | Symbol (sym) -> lookup sym env
+    | List (h :: t) ->
+        match eval h env with 
+        | Function (f) -> f t env
+        | _ -> h |> to_str |> sprintf "Expression %s is not callable" |> failwith
+    | _ -> failwith "Uh oh"
+
+let mapeval exps env =
+    List.map (fun exp -> eval exp env) exps
+
+let error func =
+    sprintf "Error in function %s" func |> failwith
+
+let car args env =
+    match mapeval args env with
+    | [List (h :: t)] -> h
+    | _ -> error "car"
+
+let cdr args env =
+    match mapeval args env with
+    | [List (h :: t)] -> List (t)
+    | _ -> error "cdr"
+
+let is_list args env =
+    mapeval args env |> (function [List (_)] -> true | _ -> false) |> Boolean
+
+let list args env =
+    List (mapeval args env)
+
+let is_null args env =
+    mapeval args env |> (function [List (l)] -> List.isEmpty l | _ -> false) |> Boolean
+
+let length args env =
+    match mapeval args env with
+    | [List (l)] -> Number (List.length l)
+    | _ -> error "length"
+
+let quote args env =
+    match args with
+    | [e] -> e
+    | _ -> error "quote"
+
+let cons args env =
+    match mapeval args env with
+    | [a; List (b)] -> List ([a] @ b)
+    | _ -> error "cons"
+
+let append args env =
+    match mapeval args env with
+    | [List (a); List (b)] -> List (a @ b)
+    | _ -> error "append"
+
+let if' args env =
+    match args with
+    | [cond; then'] -> 
+        match eval cond env with
+        | Boolean (b) -> if b then eval then' env else nil
+        | _ -> eval then' env
+    | [cond; then'; else'] ->
+        match eval cond env with
+        | Boolean (b) -> eval (if b then then' else else') env
+        | _ -> eval then' env
+    | _ -> error "if"
+
+let math args env op =
+    match mapeval args env with
+    | Number (n) :: nbs ->
+        let f x = function Number (y) -> op x y | _ -> error (op.ToString ())
+        Number (List.fold f n nbs)
+    | _ -> error (op.ToString ())
+
+let not args env =
+    match mapeval args env with
+    | [Boolean (b)] -> Boolean (not (b))
+    | [e] -> Boolean (false)
+    | _ -> error "not"
+
+let define args env =
+
+    let update_frame sym value env =
+        let frame = List.head env
+        let val' = ref (eval value env)
+        frame := Map.add sym val' frame.Value
+
+    match args with
+    | [Symbol (sym); value] ->
+        update_frame sym value env
+        nil
+    | List (Symbol (sym) :: parms) :: exps ->
+        let l = List ([Symbol (lookup_symbol ("lambda")); List (parms); List ([Symbol (lookup_symbol ("begin"))] @ exps)])
+        update_frame sym l env
+        nil
+    | _ -> error "define"
+
+let set args env =
+    match args with
+    | [Symbol (s); v] ->
+        let ref = lookupRef s env
+        ref := eval v env
+        nil
+    | _ -> error "set!"
+
+let begin' args env =
+    mapeval args env |> List.rev |> List.head
+
+let eval' args env =
+    match mapeval args env with
+    | [e] -> eval e env
+    | _ -> error "eval"
+
+let apply args env =
+    match mapeval args env with
+    | Function (f) :: a -> f a env
+    | _ -> error "apply"
+
+let lambda args env =
+    match args with
+    | [List (parms); body] ->
+        let lparms = parms |> List.map (function Symbol (s) -> s | _ -> error "lambda")
+
+        Function (fun a e ->
+            if List.length lparms <> List.length a then failwith "Argument mismatch"
+
+            let bindings = mapeval a e |> List.map ref |> List.zip lparms |> Map.ofList
+            let lenv = appendFrame bindings env
+
+            eval body lenv
+        )
+    | _ -> error "lambda"
+
+let compare args env op =
+    match mapeval args env with
+    | [Number (l); Number (r)] -> Boolean (op l r)
+    | _ -> error (op.ToString ())
+
+let display args env =
+    match mapeval args env with
+    | [e] ->
+        Console.WriteLine (to_str (e))
+        nil
+    | _ -> error "display"
+
+let global_env =
+    [[
+        "+", Function (fun args env -> math args env (+))
+        "-", Function (fun args env -> math args env (-))
+        "*", Function (fun args env -> math args env (*))
+        "/", Function (fun args env -> math args env (/))
+        "<", Function (fun args env -> compare args env (<))
+        ">", Function (fun args env -> compare args env (>))
+        "<=", Function (fun args env -> compare args env (<=))
+        ">=", Function (fun args env -> compare args env (>=))
+        "append", Function (append)
+        "apply", Function (apply)
+        "begin", Function (begin')
+        "car", Function (car)
+        "cdr", Function (cdr)
+        "cons", Function (cons)
+        "define", Function (define)
+        "display", Function (display)
+        "eval", Function (eval')
+        "if", Function (if')
+        "lambda", Function (lambda)
+        "length", Function (length)
+        "list", Function (list)
+        "list?", Function (is_list)
+        "not", Function (not)
+        "null?", Function (is_null)
+        "quote", Function (quote)
+        "set!", Function (set)
+    ] |> List.map (fun (n, f) -> (lookup_symbol (n), ref (f))) |> Map.ofList |> ref]
